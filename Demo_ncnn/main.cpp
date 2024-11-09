574a6901a689e791bf7fae8955930ea44aa21e63
JustinFung <835776509@qq.com>
2024-11-09 13:54:34 +0800

    b64 encoder

--- Demo_ncnn/main.cpp
+++ Demo_ncnn/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <random>
 #include <stdlib.h>
+#include <stdint.h>
 #include <chrono>
+#include <vector>
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
@@ -10,15 +12,54 @@
 static std::default_random_engine      generator;
 static std::normal_distribution<float> distribution(0.0, 1.0);
 
-static const float s_mean[3] = {-1.0f, -1.0f, -1.0f};
-static const float s_norm[3] = {127.5f, 127.5f, 127.5f};
+static const float s_mean[3] = {  -1.0f,  -1.0f,  -1.0f };
+static const float s_norm[3] = { 127.5f, 127.5f, 127.5f };
+
+static const char  s_b64_tab[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                                  'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+                                  'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                                  'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
+static std::string b64_encode(std::vector<uchar> &buf)
+{
+    std::string output("");
+
+    int padding = buf.size() % 3;
+    for (size_t i = 0; i < (buf.size() - 3 + padding); i += 3) {
+        uint8_t char1 = ( buf[i]     & 0b11111100) >> 2;
+        uint8_t char2 = ((buf[i]     & 0b00000011) << 4) | ((buf[i + 1] & 0b11110000) >> 4) & 0b111111;
+        uint8_t char3 = ((buf[i + 1] & 0b00001111) << 2) | ((buf[i + 2] & 0b11000000) >> 6) & 0b111111;
+        uint8_t char4 =   buf[i + 2] & 0b00111111;
+
+        output += s_b64_tab[char1];
+        output += s_b64_tab[char2];
+        output += s_b64_tab[char3];
+        output += s_b64_tab[char4];
+    }
+
+    switch (padding) {
+        case 2:
+            output += s_b64_tab[ buf[buf.size() - 1] >> 2];
+            output += s_b64_tab[(buf[buf.size() - 1] & 0b11) << 4];
+            output += "==";
+            break;
+        case 1:
+            output += s_b64_tab[(  buf[buf.size() - 2] & 0b11111100) >> 2];
+            output += s_b64_tab[(((buf[buf.size() - 2] & 0b00000011) << 4) | ((buf[buf.size() - 1] & 0b11110000) >> 4)) & 0b111111];
+            output += s_b64_tab[(  buf[buf.size() - 1] & 0b00001111) << 2];
+            output += "=";
+        case 0:
+            break;
+    }
+
+    return output;
+}
 
 static ncnn::Mat randn_ncnn(int w, int h, int c)
 {
     ncnn::Mat mat(w, h, c, (size_t)4);
     memset(mat.data, 0.f, w * h * c * 4);
 
-    #pragma omp parallel for num_threads(3)
+    #pragma omp parallel for num_threads(4)
     for (int k = 0; k < c; k++) {
         float *c_ptr = mat.channel(k);
         for (int j = 0; j < h; j++) {
@@ -49,20 +90,30 @@ int main(int argc, char **argv)
     twne.load_param("../weight/TWNE_MINI.ncnn.param");
     twne.load_model("../weight/TWNE_MINI.ncnn.bin");
 
-    std::system("mkdir images");
     for (int i = 0; i < num_generate; i++) {
+        std::string name = std::to_string(i);
+
         ncnn::Extractor ex    = twne.create_extractor();
         ncnn::Mat       noise = randn_ncnn(1, 1, 512);
+        ncnn::Mat       outputs;
 
         ex.input("in0", noise);
-        ncnn::Mat outputs;
         ex.extract("out0", outputs);
         outputs.substract_mean_normalize(s_mean, s_norm);
 
         cv::Mat image(256, 256, CV_8UC3);
         outputs.to_pixels(image.data, ncnn::Mat::PIXEL_BGR2RGB);
-        cv::imwrite("images/sample" + std::to_string(i + 1) + ".png", image);
-        std::cout << "Write sample " << i + 1 << std::endl;
+        cv::imwrite(name + ".png", image);
+
+        std::vector<uchar> buf;
+        cv::imencode(".png", image, buf);
+        std::string b64_string = b64_encode(buf);
+        FILE       *b64_file   = fopen((name + "b64").c_str(), "wb");
+        if (b64_file) {
+            fwrite(b64_string.c_str(), 1, b64_string.size(), b64_file);
+            fclose(b64_file);
+        }
+        std::cout << b64_string << std::endl;
     }
     return 0;
 }
